414c97b3806d177575273b3c366d4552f2121b6f
Margaux Rojat <margaux.rojat@gmail.com>
2025-11-07 12:11:55 +0100

    taxo

--- code/manage_taxonomic_names.R
+++ code/manage_taxonomic_names.R
@@ -27,3 +27,28 @@ temp <- classification(corres_verif$currentName, db = "wiki")
 temp_gbif <- classification(corres_verif$currentName, db = "gbif")
 temp_itis <- classification(corres_verif$matchedCanonicalSimple, db = "itis")
 
+rbindlist(temp_itis)
+
+tmp_tot <- data.frame(name = NA, 
+                      rank = NA,
+                      id = NA,
+                      sp = NA)
+
+for (n in 1:20){
+  
+  if (!is.logical(temp_itis[[n]])){
+    
+    tmp <- temp_itis[[n]]
+    tmp$sp <- names(temp_itis)[[n]]
+    tmp_tot <- rbind(tmp_tot, tmp)
+    
+  }
+  
+}
+
+tmp_tot <- na.omit(tmp_tot)
+
+classif_clean <- pivot_wider(tmp_tot, names_from = "rank", values_from = c("name", "id"))
+
+write.csv(classif_clean, file = "data/taxonomic_coverage.csv", row.names = F)
+
