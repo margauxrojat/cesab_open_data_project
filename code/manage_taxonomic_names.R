b0f1f87d579f9f72b2f91d87b71f31d4f3145a5a
Margaux Rojat <margaux.rojat@gmail.com>
2025-11-07 11:46:09 +0100

    script classif taxo

--- code/manage_taxonomic_names.R
+++ code/manage_taxonomic_names.R
@@ -2,11 +2,12 @@
 
 ## librairies ####
 library(taxize)
+library(tidyr)
 
 ## import data ####
 corres <- read.csv(file = "data/correspondance_codes_sp_noms_bino.csv")
 
-# introduce errors ####
+## introduce errors ####
 corres$nom_dummy <- corres$nom_binomial
 
 # une lettre en plus
@@ -16,4 +17,13 @@ corres[corres$tax_id == "COPBOR", "nom_dummy"] <- "Coptosperma  borbonicum"
 # un synonyme
 corres[corres$tax_id == "PSICAT", "nom_dummy"] <- "Psidium cattleianum"
 
-apg()
+## comparaison taxonomique ####
+
+# Taxize
+corres_verif <- taxize::gna_verifier(corres$nom_dummy)
+# corres <- left_join(corres, corres_verif)
+
+temp <- classification(corres_verif$currentName, db = "wiki")
+temp_gbif <- classification(corres_verif$currentName, db = "gbif")
+temp_itis <- classification(corres_verif$matchedCanonicalSimple, db = "itis")
+
